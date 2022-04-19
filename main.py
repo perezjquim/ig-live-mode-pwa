2e7b0dc2f12eb8f6964abc939df82132ac5a7908
Joaquim Perez <jquim1@hotmail.com>
2022-04-19 16:30:38 +0100

    root index.html fix

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from flask import Flask
 app = Flask( __name__, static_folder='public', static_url_path='' )
 
 @app.route( '/' )
-def static_file(path):
+def root():
     return app.send_static_file( 'index.html' )
