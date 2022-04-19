5e7481bdd301c9c8fe36e87359fcf72ee71cb85a
Joaquim Perez <jquim1@hotmail.com>
2022-04-19 16:25:45 +0100

    serve index.html as root

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import gunicorn #dummy
 
 from flask import Flask
-app = Flask( __name__, static_folder='public', static_url_path='' ) 
+app = Flask( __name__, static_folder='public', static_url_path='' )
+
+@app.route( '/' )
+def static_file(path):
+    return app.send_static_file( 'index.html' )
